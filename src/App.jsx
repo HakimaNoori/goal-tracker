e4e474cffdfb12747d6fac0341b7c08929ff1caf
Hakima Noori <hakima.noori.1379@gmail.com>
2026-03-29 10:54:27 +0430

    install react and add pages and components

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Goal Tracker</h1>
+      <p>Track your goals and progress</p>
+    </div>
+  )
+}
+
+export default App
